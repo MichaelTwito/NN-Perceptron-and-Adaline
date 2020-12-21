7a78e979eb5e8bff576b3f1eacb856cec47fd62d
Timor <xposionn@gmail.com>
2020-12-21 17:33:05 +0200

    implemented preceptron.

--- Algorithms/Perceptron.py
+++ Algorithms/Perceptron.py
@@ -9,7 +9,7 @@ class Perceptron(object):
 
     def __init__(self, dim=2, eta=1, max_epoch=None):
         self.dim = dim
-        self.W = np.zers(dim)
+        self.W = np.zeros(dim)
         self.b = np.zeros(1)
         self.eta = eta
         self.max_epoch = max_epoch
@@ -17,16 +17,12 @@ class Perceptron(object):
     def train(self, data, label, logs=False):
         i = 0
         count = 0
-        epoch = 0
         finished = True
-        while count != data.shape[0] and (self.max_epoch is None or epoch > self.max_epoch):
+        while count != data.shape[0] and self.max_epoch is None:
             count += 1
-            if label[i] * (np.sum(self.W * data[i, :], axis=-1) + self.b) <= 0:
+            if label[i] * (np.sum(self.W * data[i, :], axis=-1) + self.b)[0] <= 0:
                 self.W += self.eta * label[i] * data[i, :]
                 self.b += self.eta * label[i]
-                epoch += 1
-                if logs:
-                    print("Epoch:", epoch, ": Weights", self.W, " Bias:", self.b)
             i = (i + 1) % data.shape[0]
         print("train")
 
--- Utils/DataSetParser.py
+++ Utils/DataSetParser.py
@@ -8,15 +8,17 @@ class parser(object):
     then rerutrn a dictionary where id's are the keys, 
     and the examples will be the corresponded arrays
     """
+
     def __init__(self, dataset_filename):
         self.data_frame = pandas.read_csv(dataset_filename, header=None)
         self._normalized_dataset = self._normalize_dataset()
-        
+
     def __call__(self):
         return self._normalized_dataset
-        
 
     def _normalize_dataset(self):
-        self.data_frame[1] = self.data_frame[1].replace(['R'],1)
-        self.data_frame[1] = self.data_frame[1].replace(['N'],-1)
+        self.data_frame[1] = self.data_frame[1].replace(['R'], 1)
+        self.data_frame[1] = self.data_frame[1].replace(['N'], -1)
+        self.data_frame[self.data_frame.shape[1] - 1] = self.data_frame[self.data_frame.shape[1] - 1].replace(['?'], 0)
+
         return self.data_frame.set_index(0).T.to_dict('list')
