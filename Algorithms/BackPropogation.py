7684c17f5332c7ffc5cdbe7f7bee634fc009f583
Michael <michael.twito@annatel.net>
2020-12-23 17:06:00 +0200

    backpropagation added

---
+++ Algorithms/BackPropogation.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+class BackPropogation(object):
+    """
+        This class represents an Perceptron algorithm.
+    """
+    def __init__(self,hiddenSize):
+        #parameters
+        self.inputSize = 33
+        self.outputSize = 1
+        self.hiddenSize = hiddenSize
+        
+        #weights
+        self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # (33x2) weight matrix from input to hidden layer
+        self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (35x1) weight matrix from hidden to output layer
+        
+    def feedForward(self, X):
+        #forward propogation through the network
+        self.z = np.dot(X, self.W1) #dot product of X (input) and first set of weights (3x2)
+        self.z2 = self.sigmoid(self.z) #activation function
+        self.z3 = np.dot(self.z2, self.W2) #dot product of hidden layer (z2) and second set of weights (3x1)
+        output = self.sigmoid(self.z3)
+        return output
+        
+    def sigmoid(self, s, deriv=False):
+        if (deriv == True):
+            return 0.5*(1 + self.sigmoid(s))*(1 - self.sigmoid(s))
+        return (2/(1 + np.exp(-s))) - 1
+    
+    def backward(self, X, y, output):
+        #backward propogate through the network
+
+        self.output_error = y - output # error in output
+        self.output_delta = self.output_error * self.sigmoid(output, deriv=True)
+
+        self.z2_error = self.output_delta.dot(self.W2.T) #z2 error: how much our hidden layer weights contribute to output error   
+        self.z2_delta = self.z2_error * self.sigmoid(self.z2, deriv=True) #applying derivative of sigmoid to z2 error
+        
+        self.W1 += X.T.dot(self.z2_delta) # adjusting first set (input -> hidden) weights
+        self.W2 += self.z2.T.dot(self.output_delta) # adjusting second set (hidden -> output) weights
+        
+    def train(self, X, y):
+        output = self.feedForward(X)
+        self.backward(X, y, output)
+        
--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from Algorithms.Perceptron import Perceptron
+from Algorithms.BackPropogation import BackPropogation
 from Utils.DataSetParser import parser
 from Utils.DataSetHandler import handler
+from Utils.Score import Score
+import numpy as np
 
+def run_backpropagation(train_unlabled,train_label,test_unlabled,test_label,iterations=10000, hidden_layer_size=33):
+    train_label = [[train_label] for train_label in train_label]
+    
+    backpropogation = BackPropogation(hidden_layer_size)
+
+    for i in range(iterations):
+        backpropogation.train(train_unlabled, train_label)
+   
+    
+    result = backpropogation.feedForward(test_unlabled)
+    result_flattered = np.concatenate(result).ravel().tolist()
+    
+    scoring = Score(result_flattered, test_label)
+
+    true_positive_counter, true_negative_counter, false_positive_counter, false_negative_counter = scoring.get_score()
+    true_positive_precent, true_negative_precent, false_positive_precent, false_negative_precent = scoring.get_normallize_score()
+
+    print ("True positive: {}".format(true_positive_counter) + " precent: {}%".format(true_positive_precent))
+    print ("True negative: {}".format(true_negative_counter)+ " precent: {}%".format(true_negative_precent))
+    print ("False positive: {}".format(false_positive_counter)+ " precent: {}%".format(false_positive_precent))
+    print ("False negative: {}".format(false_negative_counter)+ " precent: {}%".format(false_negative_precent))
+    print ("Success rate: {}%\n\n".format(true_positive_precent + false_negative_precent))    
 
 def omit_keys(dataset_dictionary):
     return list(map(lambda key:dataset_dictionary[key], dataset_dictionary.keys()))
@@ -19,20 +44,31 @@ def main():
     
     plain_dataset = handler(dataset_as_list)
     
-    train_dataset, test_dataset = plain_dataset.get_train_and_test_data_by_ratio(0.66)
+    ratio = 0.66
+    print ("Dataset ratio: {}% for train, the rest for test".format(ratio*100))
 
-    train_unlabeled = plain_dataset.extract_features_from_labeled_data(train_dataset)
+    train_dataset, test_dataset = plain_dataset.get_train_and_test_data_by_ratio(ratio)
+
+    train_unlabled = plain_dataset.extract_features_from_labeled_data(train_dataset)
     test_unlabled = plain_dataset.extract_features_from_labeled_data(test_dataset)
     train_label = plain_dataset.extract_label_from_data(train_dataset)
     test_label = plain_dataset.extract_label_from_data(test_dataset)
-    perceptron = Perceptron(dim=len(train_unlabeled[0]))
+    # perceptron = Perceptron(dim=len(train_unlabled[0]))
+    
+    iterations = 10000
+    hidden_layer_size = 25
+    for j in range(1,10):
 
-    perceptron.train(data = train_unlabeled,label=train_label,logs=True)
-    print (len(train_dataset))
+        print ("---------------------------------------------------------------------------------------------\n",
+                "Iterations number: {}".format(iterations) + " Hidden layer size: {}\n".format(hidden_layer_size),
+               "---------------------------------------------------------------------------------------------\n")
 
-    print (len(test_dataset))
+        for i in range(3):
+            iterations = j * 10000 
+            hidden_layer_size = j + 24
+            run_backpropagation(train_unlabled,train_label,test_unlabled,test_label,iterations, hidden_layer_size)
 
 
-
+   
 if __name__ == "__main__":
     main()
