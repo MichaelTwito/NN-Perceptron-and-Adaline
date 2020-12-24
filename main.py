eed8ced454ce083eec6f32c08ffcde608f607685
Michael <michael.twito@annatel.net>
2020-12-24 12:04:49 +0200

    clean BP scripts

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def run_backpropagation(train_unlabled,train_label,test_unlabled,test_label,iter
     
     backpropogation = BackPropogation(hidden_layer_size)
 
+    
     for i in range(iterations):
         backpropogation.train(train_unlabled, train_label)
    
@@ -22,11 +23,39 @@ def run_backpropagation(train_unlabled,train_label,test_unlabled,test_label,iter
     true_positive_counter, true_negative_counter, false_positive_counter, false_negative_counter = scoring.get_score()
     true_positive_precent, true_negative_precent, false_positive_precent, false_negative_precent = scoring.get_normallize_score()
 
-    print ("True positive: {}".format(true_positive_counter) + " precent: {}%".format(true_positive_precent))
-    print ("True negative: {}".format(true_negative_counter)+ " precent: {}%".format(true_negative_precent))
-    print ("False positive: {}".format(false_positive_counter)+ " precent: {}%".format(false_positive_precent))
-    print ("False negative: {}".format(false_negative_counter)+ " precent: {}%".format(false_negative_precent))
-    print ("Success rate: {}%\n\n".format(true_positive_precent + false_negative_precent))    
+    number_of_successes = true_positive_precent + false_negative_precent
+
+    if (true_positive_counter + true_negative_counter + false_positive_counter + false_negative_counter == len(test_label)):
+        if (true_positive_counter + false_positive_counter !=  len(test_label) and true_negative_counter + false_negative_counter !=  len(test_label)):
+            print ("True positive: {}".format(true_positive_counter) + " precent: {}%".format(true_positive_precent))
+            print ("True negative: {}".format(true_negative_counter)+ " precent: {}%".format(true_negative_precent))
+            print ("False positive: {}".format(false_positive_counter)+ " precent: {}%".format(false_positive_precent))
+            print ("False negative: {}".format(false_negative_counter)+ " precent: {}%".format(false_negative_precent))
+            print ("Success rate: {}%\n\n".format(true_positive_precent + false_negative_precent)) 
+        else:
+            number_of_successes = -1
+    else: 
+        number_of_successes = -1 
+    
+    return number_of_successes
+
+def run_backpropagation_with_shuffled_data(number_of_runnings,plain_dataset,ratio=0.66,iterations=10000,hidden_layer_size=33):
+    print ("Dataset ratio: {}% for train, the rest for test".format(ratio*100))
+    total_runs_scoring = 0
+    good_results_counter = 0
+    for i in range(number_of_runnings):
+        train_dataset, test_dataset = plain_dataset.get_train_and_test_data_by_ratio(ratio)
+        train_unlabled = plain_dataset.extract_features_from_labeled_data(train_dataset)
+        test_unlabled = plain_dataset.extract_features_from_labeled_data(test_dataset)
+        train_label = plain_dataset.extract_label_from_data(train_dataset)
+        test_label = plain_dataset.extract_label_from_data(test_dataset)
+        number_of_successes = run_backpropagation(train_unlabled,train_label,test_unlabled,test_label,iterations, hidden_layer_size)  
+        
+        if (number_of_successes != -1):
+            total_runs_scoring += number_of_successes
+            good_results_counter += 1
+
+    print ("Average succsess rate: {}%".format(total_runs_scoring/good_results_counter))
 
 def omit_keys(dataset_dictionary):
     return list(map(lambda key:dataset_dictionary[key], dataset_dictionary.keys()))
@@ -35,18 +64,13 @@ def omit_keys(dataset_dictionary):
 def main():
     dataset_filename = 'Dataset/wpbc.data'
     normalized_dataset = parser(dataset_filename)
-    
-    # This omi_keys doesn't suppose to be here,
-    # I want to put it inside the Parser
-    # but got some shit errors with the object.
 
     dataset_as_list = omit_keys(normalized_dataset())
     
     plain_dataset = handler(dataset_as_list)
     
     ratio = 0.66
-    print ("Dataset ratio: {}% for train, the rest for test".format(ratio*100))
-
+    
     train_dataset, test_dataset = plain_dataset.get_train_and_test_data_by_ratio(ratio)
 
     train_unlabled = plain_dataset.extract_features_from_labeled_data(train_dataset)
@@ -54,20 +78,11 @@ def main():
     train_label = plain_dataset.extract_label_from_data(train_dataset)
     test_label = plain_dataset.extract_label_from_data(test_dataset)
     # perceptron = Perceptron(dim=len(train_unlabled[0]))
-    
-    iterations = 10000
-    hidden_layer_size = 25
-    for j in range(1,10):
+    # perceptron.train(data = train_unlabled,label=train_label,logs=True)
+    numer_of_runnings = 50
+    run_backpropagation_with_shuffled_data(numer_of_runnings,plain_dataset)
 
-        print ("---------------------------------------------------------------------------------------------\n",
-                "Iterations number: {}".format(iterations) + " Hidden layer size: {}\n".format(hidden_layer_size),
-               "---------------------------------------------------------------------------------------------\n")
-
-        for i in range(3):
-            iterations = j * 10000 
-            hidden_layer_size = j + 24
-            run_backpropagation(train_unlabled,train_label,test_unlabled,test_label,iterations, hidden_layer_size)
-
+   
 
    
 if __name__ == "__main__":
